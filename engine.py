aef8e769b66e201da84cf58934b6e4d68008e855
alux444 <aluux999@gmail.com>
2023-07-15 23:27:10 +1200

    FOV added

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from map import GameMap
 from entity import Entity
@@ -20,6 +21,7 @@ class Engine:
         self.event_handler = event_handler
         self.map = map
         self.player = player
+        self.update_fov()
 
     def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
@@ -30,11 +32,21 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov()
+
+    def update_fov(self) -> None:
+        self.map.visible[:] = compute_fov(
+            self.map.tiles["transparent"], (self.player.x, self.player.y), radius=8
+        )
+
+        self.map.explored |= self.map.visible
+
     def render(self, console: Console, context: Context) -> None:
         self.map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, entity.color)
+            if self.map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
