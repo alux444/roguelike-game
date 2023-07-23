9bd4d2a45a83651b7814ccc92e23b6ca5baa8b9b
alux444 <aluux999@gmail.com>
2023-07-23 13:41:09 +1200

    bugfix

--- procgen.py
+++ procgen.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Tuple, Iterator, List, TYPE_CHECKING
+from typing import Dict, Tuple, Iterator, List, TYPE_CHECKING
 from map import GameMap
 import entity_factory
 import random
@@ -8,10 +8,25 @@ import tcod
 
 if TYPE_CHECKING:
     from engine import Engine
+    from entity import Entity
 
 max_items_floor = [(1, 1), (4, 2)]
 
 max_mobs_floor = [(1, 2), (4, 3), (6, 5)]
+
+item_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factory.health_potion, 35)],
+    1: [(entity_factory.confusion_scroll, 10)],
+    2: [(entity_factory.lightning_scroll, 25)],
+    3: [(entity_factory.bomb, 25)],
+}
+
+enemy_chances: Dict[int, List[Tuple[Entity, int]]] = {
+    0: [(entity_factory.rat, 80)],
+    2: [(entity_factory.frog, 15)],
+    4: [(entity_factory.frog, 30)],
+    6: [(entity_factory.frog, 50)],
+}
 
 
 def get_max_val_for_floor(
@@ -26,6 +41,33 @@ def get_max_val_for_floor(
             current = value
 
     return current
+
+
+def get_random_entity(
+    weighted_chance_floor: Dict[int, List[Tuple[Entity, int]]],
+    number_of_mobs: int,
+    floor: int,
+) -> List[Entity]:
+    entity_weighted_chances = {}
+
+    for key, values in weighted_chance_floor.items():
+        if key > floor:
+            break
+        else:
+            for value in values:
+                entity = value[0]
+                weighted_chance = value[1]
+
+                entity_weighted_chances[entity] = weighted_chance
+
+    entities = list(entity_weighted_chances.keys())
+    entity_weighted_chance_vals = list(entity_weighted_chances.values())
+
+    chosen_entities = random.choices(
+        entities, weights=entity_weighted_chance_vals, k=number_of_mobs
+    )
+
+    return chosen_entities
 
 
 class RectangularRoom:
@@ -76,30 +118,15 @@ def place_entities(room: RectangularRoom, dungeon: GameMap, floor: int) -> None:
     number_of_mobs = random.randint(0, get_max_val_for_floor(max_mobs_floor, floor))
     number_of_items = random.randint(0, get_max_val_for_floor(max_mobs_floor, floor))
 
-    for _ in range(number_of_mobs):
+    monsters: List[Entity] = get_random_entity(enemy_chances, number_of_mobs, floor)
+    items: List[Entity] = get_random_entity(item_chances, number_of_items, floor)
+
+    for entity in monsters + items:
         x = random.randint(room.x1 + 1, room.x2 - 1)
         y = random.randint(room.y1 + 1, room.y2 - 1)
 
         if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
-            if random.random() < 0.8:
-                entity_factory.rat.spawn(dungeon, x, y)
-            else:
-                entity_factory.frog.spawn(dungeon, x, y)
-
-    for _ in range(number_of_items):
-        x = random.randint(room.x1 + 1, room.x2 - 1)
-        y = random.randint(room.y1 + 1, room.y2 - 1)
-
-        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
-            chance = random.random()
-            if chance < 0.7:
-                entity_factory.health_potion.spawn(dungeon, x, y)
-            elif chance < 0.8:
-                entity_factory.bomb.spawn(dungeon, x, y)
-            elif chance < 0.9:
-                entity_factory.confusion_scroll.spawn(dungeon, x, y)
-            else:
-                entity_factory.lightning_scroll.spawn(dungeon, x, y)
+            entity.spawn(dungeon, x, y)
 
 
 def generate_dungeon(
