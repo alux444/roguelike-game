1463a648e7d5cd9d937b65bcb69186022d86b919
alux444 <aluux999@gmail.com>
2023-07-23 13:58:58 +1200

    equipment class

---
+++ components/equipment.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from typing import Optional, TYPE_CHECKING
+
+from components.base_component import BaseComponent
+from equipment_types import EquipmentType
+
+if TYPE_CHECKING:
+    from entity import Actor, Item
+
+
+class Equipment(BaseComponent):
+    parent: Actor
+
+    def __init__(self, weapon: Optional[Item] = None, shield: Optional[Item] = None):
+        self.weapon = weapon
+        self.shield = shield
+
+    @property
+    def defense_bonus(self) -> int:
+        bonus = 0
+
+        if self.weapon is not None and self.weapon.equippable is not None:
+            bonus += self.weapon.equippable.defense_bonus
+
+        if self.shield is not None and self.shield.equippable is not None:
+            bonus += self.shield.equippable.defense_bonus
+
+        return bonus
+
+    @property
+    def power_bonus(self) -> int:
+        bonus = 0
+
+        if self.weapon is not None and self.weapon.equippable is not None:
+            bonus += self.weapon.equippable.power_bonus
+
+        if self.shield is not None and self.shield.equippable is not None:
+            bonus += self.shield.equippable.power_bonus
+
+        return bonus
+
+    def item_is_equipped(self, item: Item) -> bool:
+        return self.weapon == item or self.armor == item
+
+    def unequip_message(self, item_name: str) -> None:
+        self.parent.gamemap.engine.message_log.add_message(
+            f"You remove the {item_name}."
+        )
+
+    def equip_message(self, item_name: str) -> None:
+        self.parent.gamemap.engine.message_log.add_message(
+            f"You equip the {item_name}."
+        )
